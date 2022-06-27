7576eb07c9c2b28919201ca04a0b62fe8fbb1b7c
박경민 <pkm7628@gmail.com>
2022-06-27 06:54:57 +0000

    1

--- DAY1-2/Hellowworld.py
+++ DAY1-2/Hellowworld.py
@@ -1,3 +1,4 @@
+'''
 # DAY1 - 입력과 출력
 print('''helloworld''') # 출력 : print 함수, ', ''', """로 묶어도 출력 가능
 print("hello world") # 공백 삽입, 두줄에 걸쳐 출력
@@ -5,3 +6,6 @@ print("\'hello world\'") # 작은 따옴표(') 출력, \(이스케이프)없이 
 print('\"hello world\"') # 큰 따옴표(") 출력, 예외 : 작은 따옴표 안에 작은 따옴표를 포함하는 경우
 print("'\"hello world\"\'") # 큰 따옴표(") 출력
 print("hello\nworld") # 줄 바꿈 출력
+
+print("\"C:\\Download\\\'hello\tworld\'.py\")
+'''
